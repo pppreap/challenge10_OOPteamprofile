418242093f474ac832cca1ca0faf609672324b24
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-27 09:02:00 -0400

    added employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,27 @@ test('Creates Employee Section', ()=> {
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
 });
+
+test('Get Employee Name', ()=> {
+    const employee = new Employee ("Paula", 200, "paula@gmail.com");
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('Get Employee ID', ()=> {
+    const employee = new Employee ("Paula", 200, "paula@gmail.com");
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('Get Employee Email', ()=> {
+    const employee = new Employee ("Paula", 200, "paula@gmail.com");
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('Get Employee Role', ()=> {
+    const employee = new Employee ("Paula", 200, "paula@gmail.com");
+
+    expect(employee.getRole()).toEqual("Employee");
+});
