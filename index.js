338d03f34862ff37f71640c6a728bddd62709bfe
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-26 21:09:07 -0400

    added array of questions to be asked

--- index.js
+++ index.js
@@ -13,3 +13,79 @@ function init() {
 }
 
 init();
+
+const questions = [
+    {
+        type:'list',
+        name:'role',
+        message: 'What is the employee role?',
+        choices:['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type:'input',
+        name:'name',
+        message:"What is the employee's name?",
+        validate: inputName => {
+            if (inputName) {
+                return true;
+            } else {
+                console.log("Enter name of employee to proceed!!");
+                return false;
+            }          
+        }
+    },
+    {
+        type:'input',
+        name:'id',
+        message:"What is the employee's ID #?",
+        validate: inputId => {
+            if (inputId) {
+                return true;
+            } else {
+                console.log("Enter ID # of employee to proceed!!");
+                return false;
+            }        
+        }
+    },
+    {
+        type:'input',
+        name:'email',
+        message:"What is the employee's email address?",
+        validate: inputEmail => {
+            if (inputEmail.includes('@')) {
+                return true;
+            } else {
+                console.log("Enter email of employee to proceed!!");
+                return false;
+            }     
+        }
+    },
+    {
+        type:'input',
+        name:'officeNumber',
+        message:"What is the manager's office number?",
+        when:(inputOfficeNumber)=> inputOfficeNumber.role === 'Manager',
+        validate: inputOfficeNumber => {
+            if (inputOfficeNumber) {
+                return true;
+            } else {
+                console.log("Enter office numer of manager to proceed!!");
+                return false;
+            }     
+        }
+    },
+    {
+        type:'input',
+        name:'github',
+        message:"What is the employee's github username?",
+        when:(inputgithubUsername)=> inputgithubUsername.role === 'Engineer',
+        validate: inputOfficeNumber => {
+            if (inputOfficeNumber) {
+                return true;
+            } else {
+                console.log("Enter office numer of manager to proceed!!");
+                return false;
+            }     
+        }
+    },
+]
